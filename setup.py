af9d7d53ec720cfce5d57ef6d72df7f40960f34a
Cracker <85168111+cracker911181@users.noreply.github.com>
2021-09-27 00:21:50 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cracker_say',
+version='1.0',
+description='This PIP only for Encoding Cracker File',
+author='cracker911181',
+url='https://github.com/cracker911181/cracker_enc',
+packages=['cracker_say'],
+
+zip_safe=False)
